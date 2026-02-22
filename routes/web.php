f88eea0530cb75fb6d568f0753891e1e5d5fd277
Donel Vazquez Zambrano <dvzambrano@gmail.com>
2026-02-22 15:48:00 -0500

    I

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\TestController;
 use Modules\ZentroTraderBot\Http\Controllers\LandingController;
 
 Route::get('/', [LandingController::class, 'index'])->name('home');
 
-// Ruta para probar funciones de TelegramController
-Route::get('/test', [TestController::class, 'test']);
