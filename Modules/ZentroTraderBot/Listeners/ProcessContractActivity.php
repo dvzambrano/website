85578120b42bfde7f8c2fede86d54ba2d31d7db0
Donel Vazquez Zambrano <dvzambrano@gmail.com>
2026-03-16 10:27:24 -0400

    I

--- Modules/ZentroTraderBot/Listeners/ProcessContractActivity.php
+++ Modules/ZentroTraderBot/Listeners/ProcessContractActivity.php
@@ -33,6 +33,31 @@ class ProcessContractActivity
         ]);
 
         /*
+        {
+            "id": "af0c090f-1b52-4572-9c04-f79a9891a533",
+            "confirmed": true,
+            "data": {
+                "network_id": 80002,
+                "confirmed": true,
+                "tx_hash": "0x312019fc22fb9c2bd9173b3e2ecc2f27155040c2ae1c5afbca25acb09fe25100",
+                "contract": "0x8b0180f2101c8260d49339abfee87927412494b4",
+                "topic0": "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925",
+                "block_number": "35268999",
+                "timestamp": "1773665531",
+                "from_address": "0x3e254e81106e19b4c961cbc800390aed2a8fe800",
+                "decoded": {
+                    "name": "Approval",
+                    "params": {
+                        "owner": "0x3e254e81106e19b4c961cbc800390aed2a8fe800",
+                        "spender": "0xc15e5d5173966380fc2b297a59ed89019e4fea12",
+                        "value": { "phpseclib\\Math\\BigInteger": "10000" }
+                    }
+                },
+                "tenant_code": "59d5e7a3-dea0-4289-88f0-a39765f50bcf",
+                "listener": "moralis",
+                "trace_id": "af0c090f-1b52-4572-9c04-f79a9891a533"
+            }
+        }
 
 
         */
