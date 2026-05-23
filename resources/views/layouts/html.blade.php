57c096281c7b5b33fc06e244ca9562bc853d35aa
Donel Vazquez Zambrano <dvzambrano@gmail.com>
2026-05-23 11:45:39 -0400

    I

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -1,5 +1,4 @@
 @php
-    use Nwidart\Modules\Facades\Module;
     use Illuminate\Support\Facades\File;
 @endphp
 
@@ -30,36 +29,24 @@
     <!-- Tabler style -->
     <link href="{{ asset('zentro/extra/tablericons/tabler-icons.min.css') }}" rel="stylesheet">
 
-    <!-- Inclusion dinamica de recursos de los Modulos Activos -->
-    @foreach(Module::allEnabled() as $module)
+    <!-- Inclusion dinamica de recursos de los paquetes activos -->
+    @foreach(File::directories(base_path('vendor/dvzambrano')) as $packageDir)
         @php
-            $moduleName = $module->getName();
-            $moduleLower = strtolower($moduleName);
-            // Ruta física a la carpeta include
-            $path = "Modules/{$moduleName}/Resources/views/include";
-            $includePath = base_path($path);
+            $packageName = basename($packageDir);
+            $includePath = $packageDir . '/Resources/views/include';
         @endphp
 
         @if(File::isDirectory($includePath))
             @php
-                // Obtenemos todos los archivos .blade.php de forma recursiva
                 $files = File::allFiles($includePath);
             @endphp
 
             @foreach($files as $file)
                 @php
-                    // 1. Obtenemos la ruta relativa: "css/estilos.blade.php"
                     $relativePath = $file->getRelativePathname();
-                    echo "<!-- {$path}/{$relativePath} -->\n";
-
-                    // 2. Quitamos la extensión ".blade.php"
                     $viewPartial = str_replace('.blade.php', '', $relativePath);
-
-                    // 3. Convertimos barras en puntos para Laravel: "css.estilos"
                     $viewPartial = str_replace(DIRECTORY_SEPARATOR, '.', $viewPartial);
-
-                    // 4. Construimos el nombre completo: "gutotradebot::include.css.estilos"
-                    $fullViewName = "{$moduleLower}::include.{$viewPartial}";
+                    $fullViewName = "{$packageName}::include.{$viewPartial}";
                 @endphp
                 @includeIf($fullViewName)
             @endforeach
